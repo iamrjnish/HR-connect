b799b455f2afb4a9576c96c4638f41055858f1da
iamrjnish <rajnishkumaryadav21@gmail.com>
2025-04-29 14:01:21 +0530

    FUCK YOU

--- server.js
+++ server.js
@@ -1,29 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const path = require("path");
 
-const PORT = 3000;
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static('public')); // apne frontend files ke liye (index.html, chat.html)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
-
-  socket.on('joinRoom', (room) => {
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ room, username }) => {
     socket.join(room);
-    console.log(`User ${socket.id} joined room ${room}`);
+    socket.username = username;
+    socket.room = room;
   });
 
-  socket.on('chatMessage', ({ room, message }) => {
-    io.to(room).emit('chatMessage', message);
+  socket.on("chatMessage", ({ room, message, username }) => {
+    io.to(room).emit("chatMessage", { username, message });
   });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
+  socket.on("disconnect", () => {
+    if (socket.room && socket.username) {
+      io.to(socket.room).emit("chatMessage", {
+        username: "System",
+        message: `${socket.username} left the chat.`,
+      });
+    }
   });
 });
 
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
